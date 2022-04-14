9627b25870f02958335e453486a760597051ba0f
4rron <air272@live.com>
2022-04-14 04:47:04 -0400

    Removed Unnecessary Files

--- src/components/setHeader.tsx
+++
@@ -1,7 +0,0 @@
-function setHeader(newHeader: string){
-    const header = document.getElementById("header");
-    if (header === null) {}
-    else { header.innerHTML = newHeader; }
-}
-
-export default setHeader;
